143efdbd740acb7489b47a4040191d8d58109bd0
macbook <macbook@192.168.0.104>
2021-01-20 23:28:32 +0530

    this is one

--- src/main/java/com/qa/gridizen/Pages/CalenderPage.java
+++ src/main/java/com/qa/gridizen/Pages/CalenderPage.java
@@ -1,5 +1,6 @@
 package com.qa.gridizen.Pages;
 
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
@@ -9,12 +10,15 @@ import com.qa.gridigen.base.TestBase;
 public class CalenderPage extends TestBase {
 	@FindBy(xpath="//button[@type='submit']//i[@class='gd gridicon-plus']")
 	WebElement add;
+	JavascriptExecutor js;
 	
 	public CalenderPage() {
 		PageFactory.initElements(driver, this);
 	}
 
 	public void clickOnAdd() {
-		add.click();
+		js=(JavascriptExecutor)driver;
+		js.executeScript("arguments[0].click();", add);
 	}
+	
 }
--- src/test/java/com/qa/gridizen/LoginTest/AddCalenderTest.java
+++ src/test/java/com/qa/gridizen/LoginTest/AddCalenderTest.java
@@ -1,33 +1,80 @@
 package com.qa.gridizen.LoginTest;
 
+import java.io.File;
 import java.io.IOException;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 import com.qa.gridigen.base.TestBase;
 import com.qa.gridizen.Pages.ActionCenter;
 import com.qa.gridizen.Pages.AddCalender;
+import com.qa.gridizen.Pages.AddMaintenance;
 import com.qa.gridizen.Pages.CalenderPage;
 import com.qa.gridizen.Pages.LoginPage;
+import com.qa.gridizen.Pages.MaintenancePage;
 import com.qa.gridizen.Pages.Properties;
+import com.qa.gridizen.util.TestUtil;
 
 public class AddCalenderTest extends TestBase {
-	public static LoginPage loginPage1;
-	public static ActionCenter actionCenter;
-	public static Properties properties;
-	public static CalenderPage calenderPage;
-	public static AddCalender addCalender;
-		    @Test
-		     public void one() throws IOException, InterruptedException {
-		    	intialiszation();
-		    	loginPage1=new LoginPage();
-		    	calenderPage= new CalenderPage();
-		    	properties=new Properties();
-		    	addCalender= new AddCalender();
-		    	loginPage1.LoginClick();
-		    	actionCenter=new ActionCenter();
-		    	actionCenter.clickCalender();
-		    	calenderPage.clickOnAdd();
-		    	addCalender.select1();
-		    } 
+	
+		    public static LoginPage loginPage1;
+			public static ActionCenter actionCenter;
+			public static AddMaintenance addMaintenance;
+			public static MaintenancePage maintenancepage;
+			public static CalenderPage calenderPage;
+			ExtentHtmlReporter reporter=new ExtentHtmlReporter("/Users/macbook/eclipse-workspace/gridigen-1/ExtentReports/AddCalenderTest.html");
+			ExtentReports extent=new ExtentReports();
+			ExtentTest test1;
+			public static String screenShot(String methodName ) throws IOException {
+				TakesScreenshot ts=(TakesScreenshot)driver;
+		    	File src=ts.getScreenshotAs(OutputType.FILE);
+		    	String path1="/Users/macbook/eclipse-workspace/gridigen-1/images/"+methodName+".png";
+		    	File des=new File(path1);
+		    	FileUtils.copyFile(src, des);
+		    	return path1;
+		    	
+			}
+			@BeforeMethod
+			public void beforeMethod() throws IOException {
+				intialiszation();
+				loginPage1=new LoginPage();
+				loginPage1.LoginClick();
+				actionCenter=new ActionCenter();
+				addMaintenance= new AddMaintenance();
+				calenderPage=new CalenderPage();
+				extent.attachReporter(reporter);
+				actionCenter.clickCalender();
+				calenderPage.clickOnAdd();
+			}
+			@Test
+			public void one() {
+				test1=extent.createTest("one");
+				System.out.print("hai");
+			}
+			@AfterMethod
+			public void afterMethod(ITestResult result) throws IOException {
+				if(result.getStatus()==ITestResult.FAILURE) {
+					String path=AddCalenderTest.screenShot(result.getName());
+					test1.addScreenCaptureFromPath(path);
+					test1.log(Status.FAIL, result.getName());
+				}else if(result.getStatus()==ITestResult.SUCCESS) {
+					test1.log(Status.PASS, result.getName());
+				}
+				else if(result.getStatus()==ITestResult.SKIP) {
+					test1.log(Status.SKIP, result.getName());
+				}
+				extent.flush();
+				driver.quit();
+			}
 }
